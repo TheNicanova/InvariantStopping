2194cb977c7f9d49853696a5ae96753526fd351d
nicolas <notify.nicolas@gmail.com>
2023-11-13 14:38:55 -0500

    Make State mutable

--- src/state.jl
+++ src/state.jl
@@ -1,39 +1,44 @@
 """
-    State
+    mutable State
 
 
 Fields
 
     time: Represents concrete time.
-
+julia
     coord: An N dimensional coordinate.
 
-!!! comment
-    A state is an immutable object, perhaps it should be made mutable.
-
-!!! comment
-    We still debate as whether or not to include the time component inside the state object.
-
+    A state is a mutable object that contains the time and coordinates of a given realization.
 
 Examples
-```julia
+```jldoctest
 julia> state = State(0.0, (1.0,0.0))
+State{2, Float64, Float64}(0.0, (1.0, 0.0))
 
 julia> state = State(0.0, (1.0,))
+State{1, Float64, Float64}(0.0, (1.0,))
 
-julia> state = State(0.5, 1.0)
+julia> state = State(0.0, 1.0)
+State{1, Float64, Float64}(0.0, (1.0,))
 
 julia> time = get_time(state)
+0.0
 
 julia> coord = get_coord(state)
+(1.0,)
 ```
 """
-struct State{N, T <: Number, V <: Number}
+mutable struct State{N, T <: Number, V <: Number}
   time::T
   coord::NTuple{N, V}
 end
 
 
+"""
+    State(::Number, Number)
+
+Helper constructor for [`State`](@ref).
+"""
 State(time::Number, coord::Number) = State(time,(coord,))
 
 get_time(state::State) = state.time
